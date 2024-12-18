85facec3ede118c826d358235e1968a3f864f3ae
Eymen Eid <eymeneid@gmail.com>
2024-12-18 17:40:13 +0300

    fixes and features to Profile Command

--- src/commands/ProfileCommand.js
+++ src/commands/ProfileCommand.js
@@ -1,25 +1,46 @@
 import BaseCommand from "./BaseCommand.js";
 import { getUser } from "../helpers/getUser.js";
+import { View } from "../helpers/view.js";
 
 export default class ProfileCommand extends BaseCommand {
-  constructor() {
-    super("profile", "Return the user profile");
+  constructor(name = "profile", description = "Return the user profile") {
+    super(name, description);
   }
 
-  async execute(ctx) {
-    let user = await getUser(ctx.update.message.from.id);
-    let photo = user.profilePhotoUrl;
-    user = user.user;
+  async execute(ctx, args = []) {
+    try {
+      const fetchedUser = await getUser(ctx.update.message.from.id, ctx);
 
+      const photo = fetchedUser.profilePhotoId;
+      const user = fetchedUser.user;
 
-    const profileMessage = `
-**Name:** ${user.firstName} ${user.lastName || ""}
-**Username:** @${user.username || "N/A"}
-**Language:** ${user.languageCode?.toUpperCase() || ""}
-**Role:** ${user.status}
-**Points:** ${user.solutions + user.messageCount}
-    `;
+      const variables = {
+        name: `${user.firstName} ${user.lastName || ""}`,
+        username: user.username || "N/A",
+        language: user.languageCode?.toUpperCase() || "Unknown",
+        role: user.status || "Member",
+        points: user.solutions + user.messageCount || 0,
+      };
 
-    ctx.reply(profileMessage, { parse_mode: "Markdown" });
+      const profileMessage = await View.render("profile", variables);
+
+      if (photo) {
+        await ctx.replyWithPhoto(photo, {
+          caption: profileMessage,
+          parse_mode: "Markdown",
+          reply_to_message_id: ctx.message.message_id,
+        });
+      } else {
+        await ctx.reply(profileMessage, {
+          parse_mode: "Markdown",
+          reply_to_message_id: ctx.message.message_id,
+        });
+      }
+    } catch (error) {
+      console.error("Failed to execute ProfileCommand:", error);
+      await ctx.reply("An error occurred while retrieving your profile.", {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    }
   }
 }
